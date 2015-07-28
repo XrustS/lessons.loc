46fa635ce0e8adeca972e53289f324ecb12ed0e3
xrusts <xrusts@gmail.com>
2015-07-28 17:40:25 +0500

    Изменен файл view.php

--- newsmod/modules/view.php
+++ newsmod/modules/view.php
@@ -1,7 +1,16 @@
 <?php
 
-include_once __DIR__.'../functions/sql.php';
-$sql = '
-        select * from
-';
-$row = mysqlQwery();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+include_once __DIR__.'/../functions/sql.php';
+$sql = 'select * from news;';
+$row = mysql_fetchAll($sql);
+//var_dump($row);
+if(isset($_GET)&&empty($_GET['id'])){
+    
+}
+foreach($row as $item){
+   echo '<b>'.$item['title'].'</b><br/>';
+   echo $item['Text'].'<br/>';
+}
