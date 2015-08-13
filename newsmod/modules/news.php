71f935af35e86ddf6a066415622f02082bdb8bfc
xrusts <xrusts@gmail.com>
2015-08-13 22:54:27 +0500

    Изменен файл news.php

--- newsmod/modules/news.php
+++ newsmod/modules/news.php
@@ -5,6 +5,46 @@
  * Date: 12.08.2015
  * Time: 11:42
  */
+function img_resize( $tmpname, $size, $save_dir, $save_name, $maxisheight = 0 )
+{
+    $save_dir     .= ( substr($save_dir,-1) != "/") ? "/" : "";
+    $gis        = getimagesize($tmpname);
+    $type        = $gis[2];
+    switch($type)
+    {
+        case "1": $imorig = imagecreatefromgif($tmpname); break;
+        case "2": $imorig = imagecreatefromjpeg($tmpname);break;
+        case "3": $imorig = imagecreatefrompng($tmpname); break;
+        default:  $imorig = imagecreatefromjpeg($tmpname);
+    }
+
+    $x = imagesx($imorig);
+    $y = imagesy($imorig);
+
+    $woh = (!$maxisheight)? $gis[0] : $gis[1] ;
+
+    if($woh <= $size)
+    {
+        $aw = $x;
+        $ah = $y;
+    }
+    else
+    {
+        if(!$maxisheight){
+            $aw = $size;
+            $ah = $size * $y / $x;
+        } else {
+            $aw = $size * $x / $y;
+            $ah = $size;
+        }
+    }
+    $im = imagecreatetruecolor($aw,$ah);
+    if (imagecopyresampled($im,$imorig , 0,0,0,0,$aw,$ah,$x,$y))
+        if (imagejpeg($im, $save_dir.$save_name))
+            return true;
+        else
+            return false;
+}
 function print_form(){
     ?><form action="#" method="post" enctype="multipart/form-data">
         <h1>Добавление новости</h1>
@@ -37,15 +77,17 @@ function showNews(){
     }
 }
 function addNews($title, $text, $farr){
-    if($title!="" && $text!=""){
-        if(isset($farr)&&($farr['name']!='')){
-        //upload
-        //resize img
-        //
+    $type_img = array("");
+            $fname =time().".jpg";
+            $tmpf = $_FILES['pic']['tmp_name'];
+            if(!img_resize($tmpf,200,'./thumsimg/',$fname)){
+                echo "Произошла ошибка img_resize</br>";
+                var_dump($_FILES);
+            }else {
 
-        }
-    }else return false;
-
+                header('Location: index.php');
+                exit();
+            }
 }
 function delNews(){
 
