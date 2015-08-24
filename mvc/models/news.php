fa74e658deb3b163abf62bb20285c81e08a5e071
Xrust Home <xrusts@gmail.com>
2015-08-24 21:58:55 +0500

    ------

--- mvc/models/news.php
+++ mvc/models/news.php
@@ -1,19 +1,29 @@
 <?php
+require_once "./DB.class.php";
 
 abstract class News {
+    protected $idnews;
     protected $title;
     protected $text;
     protected $images;
 
 
    public function set_news($title,$text,$images = null){
-       $this->title = $title;
-       $this->text  = $text;
-       $this->images = $images;
+       // Устанавливаем значения полей новости
+       $this->title = htmlspecialchars($title);
+       $this->text  = htmlspecialchars($text);
+       $this->images = htmlspecialchars($images);
+
+
+
+
    }
    public function get_news($idnews=null){
 
     }
+   public function get_AllNews(){
+
+   }
 
 
 
