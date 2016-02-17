96c287ffc36cec8a20d879e691e1bce6f02b3199
Xrust Home <xrusts@gmail.com>
2016-02-17 18:56:02 +0500

    Add new class News

---
+++ mvc/models/News.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 17.02.2016
+ * Time: 12:03
+ */
+
+class News {
+    static function allNews($data=null){
+        if (isset($data)){
+
+        }
+    }
+
+
+}
