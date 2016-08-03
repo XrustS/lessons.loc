0c3aa929b5b873a8e8dd666f49f46e55dd127a1b
Александр Е. Кошелев <aek@office.unix-medical.ru>
2016-08-03 11:33:53 +0500

    Work will done

--- mvc/tests/initdb.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include __DIR__."/../models/db.class.php";
-
-/*$db = new DataBase();
-assert(true === $db->initDB());*/
-echo "TEST PASSED";
--- mvc/tests/view.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include __DIR__."/../models/View.class.php";
-
-$out = "<h1>Test Passed</h1>";
