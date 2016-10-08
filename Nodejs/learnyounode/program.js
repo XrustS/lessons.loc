b0148271476e965a7ca2c40c21c70288e76dd72c
Alex Koshelev <xrusts@gmail.com>
2016-10-08 16:03:45 +0500

    Приведение к порядку репозитория

--- Nodejs/learnyounode/program.js
+++
@@ -1,6 +0,0 @@
-//синхронное чтение файла
-const fs = require('fs');
-
-let buff = fs.readFileSync(process.argv[2]);
-
-console.log(buff.toString().split('\n').length - 1);
--- Nodejs/learnyounode/runPrintFiltringFiles.js
+++
@@ -1,9 +0,0 @@
-const printFiltringFiles = require('./printFiltringFilesModule');
-
-let pathDir = process.argv[2],
-    filter = process.argv[3];
-
-printFiltringFiles(pathDir, filter, (err, data) =>{
-    if(err) return   console.error('Ошибка!!! '+err);
-    data.forEach( (file) => console.log(file));
- });
